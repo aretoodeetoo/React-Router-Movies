41921dccfe4ffca6332759b81430873ba1fcbf0f
croy <roy.christie95@gmail.com>
2019-01-22 15:54:09 -0600

    Changed SavedList to NavLinks and added some basic styling

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -16,7 +16,7 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <Link to={`/movies/${movie.id}`} className="saved-movie">{movie.title}</Link>
+          <NavLink to={`/movies/${movie.id}`} className="saved-movie" style={{textDecoration: 'none', color: 'black'}} activeStyle={{fontWeight: 'bold', textDecoration: 'underline'}}>{movie.title}</NavLink>
         ))}
         <Link to="/" style={{textDecoration: 'none', color: 'black'}} className="home-button">Home</Link>
       </div>
